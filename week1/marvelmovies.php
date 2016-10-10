706c0ee1c53d49ee1e3831f7651403ffd614568a
JordanYoungman <jwyoungman96@homail.co.uk>
2016-10-10 15:15:21 +0100

    marvel database

--- week1/marvelmovies.php
+++ week1/marvelmovies.php
@@ -5,6 +5,7 @@
 <body>
 <h1>Hello !!!</h1>
     <?php
+
     $db = new mysqli(
         "eu-cdbr-azure-north-e.cloudapp.net",
         "b8e2bb8a8314c4",
@@ -12,23 +13,47 @@
         "cm3020database"
     );  //connects to database on azure
 
-    if($db>connect_errno){
-        die('Connectfailed['.$db>connect_error.']');
-    }
 
-    // create a SQL query as a string
-    $sql = "SELECT * FROM marvelmovies";
-
-    $result=mysqli_query($db,$sql);
-    while($row = $result->fetch_array())
-    {
-        $movieTitle = $row["title"];
-        echo  "<p>". $movieTitle."</p>";
-    }
-
-    $result->close();
-    //close connection to database
-    $db->close();
-    ?>
-</body>
-</html>
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}
+// create a SQL query as a string
+$sql_query = "SELECT * FROM marvelmovies";
+// execute the SQL query
+$result = $db->query($sql_query);
+// iterate over $result object one $row at a time
+// use fetch_array() to return an associative array
+echo "<p><strong>All Movies: </strong>";
+while($row = $result->fetch_array()){
+    // print out fields from row of data
+    echo $row['title'] . " | ";
+}
+echo "</p>";
+$sql_query = "SELECT * FROM marvelmovies where yearReleased < 2010";
+$result = $db->query($sql_query);
+echo "<p><strong>Before 2010: </strong>";
+while($row = $result->fetch_array()){
+    // print out fields from row of data
+    echo $row['title'] . " | ";
+}
+echo "</p>";
+$sql_query = "SELECT * FROM marvelmovies where productionStudio like 'Marvel Studios'";
+$result = $db->query($sql_query);
+echo "<p><strong>Marvel: </strong>";
+while($row = $result->fetch_array()){
+    // print out fields from row of data
+    echo $row['title'] . " | ";
+}
+echo "</p>";
+$sql_query = "SELECT * FROM marvelmovies where yearReleased > 2010";
+$result = $db->query($sql_query);
+echo "<p><strong>After 2010: </strong>";
+while($row = $result->fetch_array()){
+    // print out fields from row of data
+    echo $row['title'] . " | ";
+}
+echo "</p>";
+$result->close();
+// close connection to database
+$db->close();

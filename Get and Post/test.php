cf724f97dda9de8a01c188fe7514dcbeab4e1711
JordanYoungman <jwyoungman96@homail.co.uk>
2016-10-17 14:41:34 +0100

    test

--- Get and Post/test.php
+++ Get and Post/test.php
@@ -1,5 +1,5 @@
 <form action="formResponse.php" method="post">
-    <label>Forename</label><input type=“text” name=“forename”>
-    <label>Surname</label><input type=“text” name=“surname”>
+    <label>Forename</label><input type="text" name="forename">
+    <label>Surname</label><input type="text" name="surname">
     <input type="submit" value="Submit">
-</form> 
+</form>

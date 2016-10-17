e9457c83fc7c355f11fa2c2bbaac6c5f8637bcbf
JordanYoungman <jwyoungman96@homail.co.uk>
2016-10-17 14:49:13 +0100

    test

--- Get and Post/formResponse.php
+++ Get and Post/formResponse.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $forename = $_POST["forename"];
 $surname = $_POST["surname"];
 echo $forename . $surname;

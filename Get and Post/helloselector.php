e98d414383297e81ab0b24e63e5ae763092caae9
JordanYoungman <jwyoungman96@homail.co.uk>
2016-10-17 13:42:38 +0100

    hello planet selector

--- Get and Post/helloselector.php
+++ Get and Post/helloselector.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1607731
- * Date: 17/10/2016
- * Time: 13:34
- */
+<html>
+<head>
+    <title>
+        Where would you like to say hello?
+    </title>
+</head>
+<body>
+<p>This is a very simple HTML document</p>
+<p>It only has two paragraphs</p>
+</body>
+</html>
+<?
+$world = $_GET["world"];
+echo "<p>Hello{$world}</p>"
+?>

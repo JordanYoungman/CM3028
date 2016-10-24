ee80c36381eed9c9ee2161e59fce0a9d8cecc71c
Jordan <jwyoungman96@hotmail.co.uk>
2016-10-24 18:18:47 +0100

    update from home

--- week2/addblogpost.php
+++ week2/addblogpost.php
@@ -3,6 +3,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 // execute if requested using HTTP POST Method
     $forename = $_POST["forename"];
     $surname = $_POST["surname"];
-    print("<h1>Hello {$forename} {$surname}</h1>");
+    print("<h1> $title </h1>");
+    echo "<title>{$title}</title>";
+    echo "<p>{$content}</p>";
+    echo "<strong>{$category}</strong>";
 }
 ?>

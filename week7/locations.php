0c62a1c4a27f8623f149bf093166a69021e53e1b
JordanYoungman <jwyoungman96@homail.co.uk>
2016-11-07 14:51:38 +0000

    test

--- week7/locations.php
+++ week7/locations.php
@@ -1,4 +1,17 @@
 <?php
+$db = new mysqli(
+    "eu-cdbr-azure-north-e.cloudapp.net",
+    "b8e2bb8a8314c4",
+    "b422cf68",
+    "cm3020database"
+);  //connects to database on azure
+
+
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}
+
 $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
